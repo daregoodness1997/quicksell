184b2b3e557d3259cacf67fc768e4c52b9d127e6
Mac <mac@Macs-MacBook-Pro.local>
2022-12-08 08:29:19 +0100

    top navigation link

--- src/components/navigations/index.tsx
+++ src/components/navigations/index.tsx
@@ -1,10 +1,33 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+const navItems = [
+  { name: 'Home', link: '/' },
+  { name: 'Trade', link: '/' },
+  { name: 'Keys', link: '/' },
+  { name: 'Giveaways', link: '/' },
+  { name: 'Raffles', link: '/' },
+  { name: 'Auctions', link: '/' },
+  { name: 'Premium', link: '/' },
+  { name: 'Donation', link: '/' },
+  { name: 'About', link: '/' },
+  { name: 'Support', link: '/' },
+  { name: 'Marketplace', link: '/' },
+  { name: 'Reserving', link: '/' },
+  { name: 'Brokering', link: '/' },
+];
 
 const TopNav = () => {
   return (
     <div className='top-navigation'>
-      <div>Quicksell</div>
-      <nav>Navigation</nav>
+      <div className='left-navigation'>
+        <img src='assets/img/qc-logo.png' />
+        <nav>
+          {/* {navItems.map(item => (
+            <Link to={item.link}>{item.name}</Link>
+          ))} */}
+        </nav>
+      </div>
       <div>CTA</div>
     </div>
   );

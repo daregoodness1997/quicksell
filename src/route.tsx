686e8686f0627122c6058f0db30b82d38148ae3f
Mac <mac@Macs-MacBook-Pro.local>
2022-12-09 14:41:37 +0100

    assets folder brought into src

--- src/route.tsx
+++ src/route.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import About from './pages/about';
+import Home from './pages/home';
+import Keys from './pages/keys';
+import Support from './pages/support';
 
-const Route = () => {
-  return <div>Route</div>;
+const AppRoute = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/keys' element={<Keys />} />
+        <Route path='/support' element={<Support />} />
+      </Routes>
+    </Router>
+  );
 };
 
-export default Route;
+export default AppRoute;

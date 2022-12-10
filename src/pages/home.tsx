afd84b67369b6f8df6cafa85ad71062751d69c65
Mac <mac@Macs-MacBook-Pro.local>
2022-12-10 08:45:46 +0100

    hero-box

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import { TopNav } from '../components';
+import Hero from '../views/Hero';
 
 const Home = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Hero />
+    </div>
+  );
 };
 
 export default Home;

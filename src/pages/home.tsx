524210eb021ed24bc1f591b5d50d80a522fcf8da
Dare Goodness <daregoodness@gmail.com>
2022-12-10 09:15:53 +0100

    added product mockup section

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { TopNav } from '../components';
 import Hero from '../views/Hero';
+import Product from '../views/Product';
 
 const Home = () => {
   return (
     <div>
       <Hero />
+      <Product />
     </div>
   );
 };

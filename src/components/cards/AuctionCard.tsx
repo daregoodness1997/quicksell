ad1ba4da52f414037c83c4ad33701d9139a7ec56
Dare Goodness <daregoodness@gmail.com>
2022-12-11 11:44:40 +0100

    auction card

--- src/components/cards/AuctionCard.tsx
+++ src/components/cards/AuctionCard.tsx
@@ -6,10 +6,12 @@ const AuctionCard = () => {
       <div className='img-container'>
         <img src='assets/img/qc-logo.png' alt='' />
       </div>
+
+      {/* Start Left Content */}
       <div>
         <p className='auction-name'>Haunted Einstein</p>
 
-        <div className='flex'>
+        <div className='flex auction-flex'>
           <div>
             <small>Top Bids</small>
             <h5>10 Keys</h5>
@@ -18,12 +20,48 @@ const AuctionCard = () => {
             <small>Top Bids</small>
             <h5>10 Keys</h5>
           </div>
-          <div>
+          <div className='instant-buy'>
             <small>Top Bids</small>
             <h5>10 Keys</h5>
           </div>
         </div>
+
+        <div className='flex'>
+          <div className='flex space-between'>
+            <div className='auction-flex-sub'>
+              <small>Price steps</small>
+              <small>No</small>
+            </div>
+            <div className='auction-flex-sub'>
+              <small>Accepting</small>
+              <small>All</small>
+            </div>
+          </div>
+
+          {/* Break */}
+
+          <div className='flex space-between'>
+            <div className='auction-flex-sub'>
+              <small>Type</small>
+              <small>Fast Auction</small>
+            </div>
+
+            <div className='auction-flex-sub'>
+              <small>Duration</small>
+              <small>10 hrs</small>
+            </div>
+          </div>
+        </div>
+
+        <div className='flex space-between bottom-wrapper'>
+          <img
+            src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTg1OFGfAFrxNsDHMsG83hVqkMgqEeJC9Gj42F4GEffFQ&s'
+            alt=''
+            className='auction-avatar'
+          />
+        </div>
       </div>
+      {/* End Left Content */}
     </div>
   );
 };

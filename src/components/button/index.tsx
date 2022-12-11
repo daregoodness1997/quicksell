5c1dedad81332b98b48b1d633a90ab95c8b8d5e0
Dare Goodness <daregoodness@gmail.com>
2022-12-11 13:22:20 +0100

    buttoon hexagon

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 
 const Button = () => {
   return (
-    <>
-      <div className='btn-wrapper'>
-        <a href='#' className='btn flex' data-jzz-gui-player='true'>
+    <div className='button-wrapper'>
+      <button className='button ribbon-outset border'>
+        <div className='button-flex'>
           <img src='assets/svg/steam-icon.svg' alt='Sign in with Steam' />
           <span>Sign in with Steam</span>
-        </a>
-      </div>
-    </>
+        </div>
+      </button>
+    </div>
   );
 };
 

682a0886a2917905601fd4fc51b59bf3c3eda8b0
Dare Goodness <daregoodness@gmail.com>
2022-12-16 19:52:52 +0100

    cta section

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { TopNav } from '../components';
+import CTA from '../views/CTA';
 import Feautures from '../views/Feautures';
 import Hero from '../views/Hero';
 import Keys from '../views/Keys';
@@ -14,6 +15,7 @@ const Home = () => {
       <Feautures />
       <Keys />
       <Trades />
+      <CTA />
     </div>
   );
 };

da3e6e452e8ab108a61b776a36c1635b6e51e7b0
Dare Goodness <daregoodness@gmail.com>
2022-12-26 11:16:11 +0100

    giveaway slider

--- src/views/Feautures.tsx
+++ src/views/Feautures.tsx
@@ -44,6 +44,43 @@ const Feautures = () => {
     ],
   };
 
+  const giveawaySettings = {
+    dots: true,
+    infinite: true,
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    autoplay: true,
+    speed: 2000,
+    autoplaySpeed: 2000,
+    pauseOnHover: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 6,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
   const auctionSettings = {
     dots: true,
     infinite: true,
@@ -177,7 +214,7 @@ const Feautures = () => {
         </div>
 
         <div className='slider-box'>
-          <Slider {...auctionSettings}>
+          <Slider {...giveawaySettings}>
             <GiveawayCard />
             <GiveawayCard />
             <GiveawayCard />

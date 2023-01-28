f809d51949db317352bbb0e4de1a65d77da1f20b
Dare Goodness <daregoodness@gmail.com>
2023-01-29 00:42:41 +0100

    app button

--- src/views/modal-views/EmptyTradeCards.tsx
+++ src/views/modal-views/EmptyTradeCards.tsx
@@ -1,12 +1,11 @@
 import React, { useContext } from 'react';
 import { Button } from '../../components';
+import AppButton from '../../components/button/AppButton';
 import { ProductContext } from '../../context/AppContext';
 import { ProductContextType } from '../../types/@types.product';
 
 const EmptyTradeCards = () => {
-  const { onLoggedIn } = useContext(
-    ProductContext
-  ) as ProductContextType;
+  const { onLoggedIn } = useContext(ProductContext) as ProductContextType;
   return (
     <div className='empty-trade-wrapper empty empty-card'>
       <div className='empty-content'>
@@ -17,7 +16,7 @@ const EmptyTradeCards = () => {
 
       <img src='img/steps.png' className='step-img' />
       <div className='button-group' style={{ marginTop: '1rem' }}>
-        <Button
+        <AppButton
           label='Signup through steam'
           size='md'
           variant='solid'

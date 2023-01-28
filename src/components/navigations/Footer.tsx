63bb6b7cfadb95c266ac5088fe5ffdb2c6633a58
Dare Goodness <daregoodness@gmail.com>
2023-01-28 15:41:55 +0100

    footer copyright

--- src/components/navigations/Footer.tsx
+++ src/components/navigations/Footer.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { DiscordIcon, SteamGreyIcon } from '../icons';
 
 const footerNavigation = [
   {
@@ -18,31 +19,41 @@ const footerNavigation = [
 
 const Footer = () => {
   return (
-    <div className='container footer flex'>
-      <div className='left-navigation'>
-        <Link to='/'>
-          <img src='assets/img/qc-logo.png' />
-        </Link>
-        <p>
-          © 2019-2022 Quicksell.store - Powered by Steam - Particles from
-          backpack.tf
-        </p>
+    <div>
+      <div className='container footer flex'>
+        <div className='left-navigation'>
+          <Link to='/'>
+            <img src='assets/img/qc-logo.png' />
+          </Link>
+          <p>
+            © 2019-2022 Quicksell.store - Powered by Steam - Particles from
+            backpack.tf
+          </p>
+        </div>
+
+        <div className='right-navigation'>
+          <div className='navigation'>
+            {footerNavigation.map(({ title, links }, idx) => (
+              <div className='list' key={idx}>
+                <h5>{title}</h5>
+                <ul>
+                  {links.map((link, idx) => (
+                    <Link to={link} key={idx}>
+                      {link}
+                    </Link>
+                  ))}
+                </ul>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
 
-      <div className='right-navigation'>
-        <div className='navigation'>
-          {footerNavigation.map(({ title, links }, idx) => (
-            <div className='list' key={idx}>
-              <h5>{title}</h5>
-              <ul>
-                {links.map((link, idx) => (
-                  <Link to={link} key={idx}>
-                    {link}
-                  </Link>
-                ))}
-              </ul>
-            </div>
-          ))}
+      <div className='container flex'>
+        <p>© 2022 QuickStore. All rights reserved.</p>
+        <div className='flex gap-2'>
+          <DiscordIcon />
+          <SteamGreyIcon />
         </div>
       </div>
     </div>
